5dd44c7559176ee4a55afd4af59d982be9524123
syo <syolmm@qq.com>
2018-01-23 18:05:09 +0800

    牛客网剑指offer第一题

--- 相关试题/牛客剑指offer66.js
+++ 相关试题/牛客剑指offer66.js
@@ -6,13 +6,34 @@
  *  要查找数字比左下角数字小时，上移
  **/
 
-// target 目标元素 array 二维数组
+
+
+/**
+ *  矩阵有序的 而且二维数组元素个数相等。根据规律右上角或者左下角的元素 arr[row][col] 进行比较
+ *  当taregt 小于arr[row][col]时，target必定在元素的左边 col--;
+ *  当target 大于元素arr[row][col]。target 必定在元素数组列的下班 row++
+ **/
+
 function Find(target, array) {
-    var found = false;
-    // 数值检测 检测其是否存在
+    // 首先设置一个变量从0开始
+    var row = 0;
+    var col = 0;
+    // 如果是海量的元素 可以使用二分查找来找元素
+    var mid = 0;
+    // 类型监测
     if (array !== null && array.length > 0 && array[0].length > 0) {
-        // 保存行列的变量
-        var rows = array.length;
-        var cloums = arr;
+    	// 循环每个区间的值
+    	while (row < array.length - 1 && col >= 0) {
+            // 这里是考虑到二维数组不是一个长度相等的元素 每次都赋值
+            col = array[row].length - 1;
+            if (target === array[row][col]) {
+                return true;
+            } else if (target > array[row][col]) {
+                row++;
+            } else {
+                col--;
+            }
+    	}
     }
+    return false;
 }
