2e18fca1bd38fad68884dc5678f0b67db3db6e7a
syo <syolmm@qq.com>
2018-06-22 19:57:42 +0800

    syo

---
+++ loadsh源码/mounder.js
@@ -0,0 +1,10 @@
+;(function(){
+
+  var root = this;
+  var previousUnderscore = root._;
+
+  // 用于代替原型交换的裸函数参考
+  
+
+  // 调用this 传入window or node global
+}.call(this));
---
+++ loadsh源码/readme.md
@@ -0,0 +1,8 @@
+#### underscore 源码阅读
+
+阅读underscore源码主要是为了巩固JavaScript。会按照文档顺序进行分析。
+
+
+#### 数组篇
+
+
